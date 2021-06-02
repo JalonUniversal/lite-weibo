f0a55488e84b5f02cb2a920e43b5da6731516809
JalonUniversal <453741411@qq.com>
2021-06-03 00:45:12 +0800

    feat: template render logic feat

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,13 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: 'lwh heroman black socks',
+    blogList: [
+      { id: 1, title: 'weibo-1' },
+      { id: 2, title: 'weibo-2' },
+      { id: 3, title: 'weibo-3' },
+    ]
   })
 })
 
@@ -13,6 +19,14 @@ router.get('/string', async (ctx, next) => {
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body;
+  ctx.body = {
+    userName,
+    password
   }
 })
 
